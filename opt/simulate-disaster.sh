a998c40183c316fe5943dc583a9b2fae741447b5
Dillon Winchell <dwinchel@redhat.com>
2019-01-09 17:28:49 -0500

    Added in other commands

--- opt/simulate-disaster.sh
+++ opt/simulate-disaster.sh
@@ -3,13 +3,18 @@ while true; do
     echo
     echo
     echo Things are bad. People need help.
+
     psql -U ${POSTGRESQL_USER} -h postgresql.naps-emergency-response.svc -d ${POSTGRESQL_DATABASE} --command="update mission set current_status='Assigned' where mission_id in (select mission_id from mission where current_status='Requested' order by last_update desc limit 1);"
-
-#update mission set current_status='Pickedup' where mission_id in (select mission_id from mission where current_status='Assigned' order by last_update desc limit 20);
-#update mission set current_status='Rescued' where mission_id in (select mission_id from mission where current_status='Pickedup' order by last_update desc limit 20);
-#update mission set current_status='Cancelled' where mission_id in (select mission_id from mission where current_status='Assigned' order by last_update desc limit 10);
-#update mission set current_status='Requested' where mission_id in (select mission_id from mission where current_status='Cancelled' order by last_update desc limit 10);
-#update mission set current_status='Requested' where mission_id in (select mission_id from mission where current_status='Rescued' order by last_update desc limit 20);
-
     sleep 5
+    psql -U ${POSTGRESQL_USER} -h postgresql.naps-emergency-response.svc -d ${POSTGRESQL_DATABASE} --command="update mission set current_status='Pickedup' where mission_id in (select mission_id from mission where current_status='Assigned' order by last_update desc limit 20);"
+    sleep 5
+    psql -U ${POSTGRESQL_USER} -h postgresql.naps-emergency-response.svc -d ${POSTGRESQL_DATABASE} --command="update mission set current_status='Rescued' where mission_id in (select mission_id from mission where current_status='Pickedup' order by last_update desc limit 20);"
+    sleep 5
+    psql -U ${POSTGRESQL_USER} -h postgresql.naps-emergency-response.svc -d ${POSTGRESQL_DATABASE} --command="update mission set current_status='Cancelled' where mission_id in (select mission_id from mission where current_status='Assigned' order by last_update desc limit 10);"
+    sleep 5
+    psql -U ${POSTGRESQL_USER} -h postgresql.naps-emergency-response.svc -d ${POSTGRESQL_DATABASE} --command="update mission set current_status='Requested' where mission_id in (select mission_id from mission where current_status='Cancelled' order by last_update desc limit 10);"
+    sleep 5
+    psql -U ${POSTGRESQL_USER} -h postgresql.naps-emergency-response.svc -d ${POSTGRESQL_DATABASE} --command="update mission set current_status='Requested' where mission_id in (select mission_id from mission where current_status='Rescued' order by last_update desc limit 20);"
+    sleep 5
+
 done
